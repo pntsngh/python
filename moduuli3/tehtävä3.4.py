3f83136281aebecca2178701117b6f4f03b780ed
punee <puneet97@hotmail.com>
2022-08-31 17:35:17 +0300

    3

--- moduuli3/tehtävä3.4.py
+++ moduuli3/tehtävä3.4.py
@@ -1,5 +1,6 @@
 vuosi = int(input('Anna vuosiluku: '))
-if vuosi%4 == 0:
-    print('Karkausvuosi. ')
+if ((vuosi%400 == 0) or ((vuosi%4 == 0) and (vuosi%100 != 0))):
+    print('Karkausvuosi.')
+
 else:
     print('Ei ole karkausvuosi.')
