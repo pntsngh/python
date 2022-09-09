d481ad3617feab00ce7b1007313f87ed16cc9738
punee <puneet97@hotmail.com>
2022-09-09 13:30:11 +0300

    .

--- moduuli4/tehtävä4.4.py
+++ moduuli4/tehtävä4.4.py
@@ -5,7 +5,7 @@ oikeanumero = random.randint(1, 10)
 while True:
     arvaus = int(input('Arvaa luku väliltä 1-10: '))
     if arvaus == oikeanumero:
-        print('Oikea arvaus! Voitit pelin!')
+        print('Oikein!')
         break
     elif arvaus < oikeanumero:
         print('Luku on isompi.')
