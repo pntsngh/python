bf5c781346ecb786bcca163f05717f1307940537
punee <puneet97@hotmail.com>
2022-09-13 20:32:11 +0300

    .

---
+++ moduuli6/tehtävä6.6.py
@@ -0,0 +1,19 @@
+import math
+
+
+def neliöhinta(säde, hinta):
+    nh = math.pi * säde**2 / 100 / hinta
+    return nh
+
+r = float(input('säde: '))
+h = float(input('hinta: '))
+vastaus = neliöhinta(r, h)
+
+r2 = float(input('säde: '))
+h2 = float(input('hinta: '))
+vastaus2 = neliöhinta(r2, h2)
+
+
+
+print(vastaus)
+print(vastaus2)
