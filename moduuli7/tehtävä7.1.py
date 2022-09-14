bf0dffda0f0fab5b87b13c3f58e52ba4bfa55013
punee <puneet97@hotmail.com>
2022-09-14 15:50:12 +0300

    .

---
+++ moduuli7/tehtävä7.1.py
@@ -0,0 +1,7 @@
+vuodenajat = ('talvi','talvi','kevät','kevät','kevät','kesä','kesä','kesä','syksy',
+              'syksy','syksy','talvi')
+kuukausi = int(input('Syötä kuukauden numero: '))
+vuodenaika = vuodenajat[kuukausi-1]
+
+print(f'Vuodenaika on {vuodenaika}.')
+
