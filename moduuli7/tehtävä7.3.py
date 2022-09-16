f9bfc46a08c406a53e6bce428e6ce892b8309fe4
punee <puneet97@hotmail.com>
2022-09-16 14:41:43 +0300

    .

--- moduuli7/tehtävä7.3.py
+++ moduuli7/tehtävä7.3.py
@@ -7,19 +7,18 @@ while syöte != 'lopeta':
     if syöte == 'hae':
         haku = input('Syötä ICAO: ')
         if haku in lentokentät:
-            print({lentokentät[haku]})
+            print(lentokentät[haku])
         else:
             print('Lentokenttää ei löydy.')
-        syöte = input('Mitä haluat tehdä?(hae/lisää/lopeta): ')
 
     elif syöte == 'lisää':
         koodi = input('Syötä uusi ICAO: ')
         nimi = input('Syötä uusi lentokenttä: ')
         lentokentät[koodi] = nimi
-        syöte = input('Mitä haluat tehdä?(hae/lisää/lopeta): ')
 
     else:
         print('Virheellinen syöte.')
-        syöte = input('Mitä haluat tehdä?(hae/lisää/lopeta): ')
+
+    syöte = input('Mitä haluat tehdä?(hae/lisää/lopeta): ')
 
 print(lentokentät)
